e5300cb655657dd632cfb8168c41612ad8ed7770
sudeeptarlekar <sudeeptarlekar@gmail.com>
2025-07-20 18:03:25 +0200

    Add basic view with components

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,19 +1,25 @@
-pub mod event;
-pub mod task;
+pub mod player;
 pub mod timer;
+pub mod todo;
+
+use std::thread;
 
 use ratatui::DefaultTerminal;
 use ratatui::Frame;
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::widgets::{Block, Borders, Paragraph};
 
-use task::Task;
+use player::Player;
 use timer::Timer;
+use todo::ToDo;
 
 #[derive(Debug, Default)]
 pub struct App {
     // Is app running?
     pub running: bool,
-    pub task_list: Vec<Task>,
+    pub todo_list: ToDo,
     pub timer: Timer,
+    pub player: Player,
 }
 
 impl App {
@@ -25,14 +31,39 @@ impl App {
     }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> color_eyre::Result<()> {
-        while self.running {
+        let mut i = 0;
+        while self.running && i < 1 {
             terminal.draw(|frame| self.draw(frame))?;
-            event::handle_event()?;
+            // event::handle_event()?;
+            thread::sleep(std::time::Duration::from_secs(10));
+            i += 1;
         }
         Ok(())
     }
 
-    fn draw(&self, _frame: &mut Frame) {
-        todo!();
+    fn draw(&self, frame: &mut Frame) {
+        let outer_layouts = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(35), Constraint::Percentage(65)])
+            .split(frame.area());
+
+        let inner_layouts = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(50),
+            ])
+            .split(outer_layouts[0]);
+
+        self.player.draw(frame, inner_layouts[0]);
+        self.timer.draw(frame, inner_layouts[1]);
+        self.todo_list.draw(frame, outer_layouts[1]);
+        frame.render_widget(
+            Paragraph::new("Channel List")
+                .centered()
+                .block(Block::new().borders(Borders::all())),
+            inner_layouts[2],
+        );
     }
 }
--- src/app/timer.rs
+++ src/app/timer.rs
@@ -1,6 +1,8 @@
-use ratatui::buffer::Buffer;
-use ratatui::layout::Rect;
-use ratatui::widgets::Widget;
+use ratatui::prelude::{Buffer, Frame, Rect};
+use ratatui::style::Stylize;
+use ratatui::symbols::border::THICK;
+use ratatui::text::Line;
+use ratatui::widgets::{Borders, Paragraph, Widget, block::Block};
 
 #[derive(Default, Debug)]
 pub struct Timer {
@@ -23,6 +25,33 @@ pub enum Pomodoro {
     Long,
 }
 
-impl Widget for Timer {
-    fn render(self, _area: Rect, _buffer: &mut Buffer) {}
+impl Timer {
+    pub fn draw(&self, frame: &mut Frame, area: Rect) {
+        frame.render_widget(self, area)
+    }
+}
+
+impl Widget for &Timer {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::raw("Timer");
+        let instructions = Line::from(vec![
+            "[Start/Stop ".into(),
+            "s] ".bold().blue(),
+            "[Break ".into(),
+            "b] ".bold().blue(),
+            "[Short/Long ".into(),
+            "l]".bold().blue(),
+        ]);
+
+        let block = Block::new()
+            .borders(Borders::all())
+            .border_set(THICK)
+            .title(title.left_aligned())
+            .title_bottom(instructions.centered());
+
+        Paragraph::new("Coming Soon!")
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
